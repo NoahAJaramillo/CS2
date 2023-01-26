74b436934357c2f8d401a66bc264d1cfbc39b6d7
Noah <noahjaramillo@yahoo.com>
2023-01-26 11:30:41 -0700

    updated both mains

--- Practicums/Practicum2/main1.cpp
+++ Practicums/Practicum2/main1.cpp
@@ -6,35 +6,42 @@ void FindSmallest(int * a, int * b, int * c);
 
 int main()
 {
-    int *a, *b, *c;
-    cin >> *a >> *b >> *c;
-    cout << *a << *b << *c;
-    FindSmallest(a, b, c);
-    cout << *a << *b << *c;
+    int a, b, c;
+    int nums[a,b,c];
+    cin >> a, b, c;
+    int* A = &a;
+    int* B = &b;
+    int* C = &c;
+    FindSmallest(A, B, C);
+    for(int i = 0; i < 3; i++)
+    {
+        cout << nums[i] << endl;
+    }
     
 }
 
 void FindSmallest(int * a, int * b, int * c)
 {
+    for(int i = 0; i < 3; i++)
+    {
     int * temp;
-    if(&a > &b)
+    if(*a > *b)
     {
         temp = a;
         a = b;
         b = temp;
     }
-    if(&a > &c)
+    if(*a > *c)
     {
         temp = a;
         a = c;
         c = temp;
     }
-    if(&b > &c)
+    if(*b > *c)
     {
         temp = b;
         b = c;
         c = temp;
     }
-
-
+    }
 }
