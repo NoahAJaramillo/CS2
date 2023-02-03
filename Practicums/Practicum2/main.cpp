c9941dbe5919a732a27d5dc0133688807a6d0057
Noah <noahjaramillo@yahoo.com>
2023-02-03 11:32:42 -0700

    updated files

--- Practicums/Practicum2/main.cpp
+++ Practicums/Practicum2/main.cpp
@@ -1,46 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void FindSmallest(int* a, int* b, int* c);
+void Reverse(char* src);
 
 int main()
 {
-
-    int* a = new int, * b = new int, * c = new int;
-    cin >> *a >> *b >> *c;
-
-    int* arr[] = {a, b, c};
-    FindSmallest(a, b, c);
-
-    for(int i =0; i < 3; i++)
-    {
-        cout << *arr[i] << endl;
-    }
-
-    delete a;
-    delete b;
-    delete c;
-
+    string str;
+    getline(cin, str);
+    Reverse(&str[0]);
     return 0;
 }
 
-void FindSmallest(int* a, int* b, int* c)
+void Reverse(char* src)
 {
-    int temp;
-    for(int i = 0; i < 2; i++)
+    int length = 0;
+    char* ch = src;
+
+    while(*ch != '\0')
     {
-        if(*a > *b)
-        {
-            temp = *a;
-            *a = *b;
-            *b = temp;
-        }
-        if(*b > *c)
-        {
-            temp = *b;
-            *b = *c;
-            *c = temp;
-        }
+        ch++;
+        length++;
     }
+    
+    for(int i = length; i >= 0; i--)
+    {
+        cout << src[i];
+    }
+
 }
