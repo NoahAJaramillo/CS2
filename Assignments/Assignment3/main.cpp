93501c409bd18c907a39eaff3ce0600717df42c0
Noah <noahjaramillo@yahoo.com>
2023-04-16 15:15:12 -0600

    updated files

--- Assignments/Assignment3/main.cpp
+++ Assignments/Assignment3/main.cpp
@@ -6,43 +6,80 @@
 
 using namespace std;
 
-//#define DEBUG
+#define DEBUG_INS
+#define DEBUG_BUB
+#define DEBUG_QUICK
+
+void Test();
 
 int main()
 {
-#ifdef DEBUG
-    double arr[50];
+    int size = 50000;
+    quickComp, quickAssgn, insertComp, insertAssgn, bubSwaps = 0;
+    Test();
+    cout << "quickComp: " << quickComp << endl;
+    cout << "quickAssgn: " << quickAssgn << endl;
+    cout << "insertComp: " << insertComp << endl;
+    cout << "insertAssgn: " << insertAssgn << endl;
+}
+
+void Test()
+{
+    #ifdef DEBUG_INS
+    double array[50];
     srand(4331248);
     double x;
     for(int i = 0; i < 50; i++)
     {
         x = rand() % 99900 + 100;
-        arr[i] = x;
+        array[i] = x;
+    }
+    cout << "presort" << endl;
+    for(int i = 0; i < 50; i++)
+    {
+        cout << i << ": " << array[i] << endl;
+    }
+    InsertionSort(array, 50);
+    cout << "postsort" << endl;
+    for(int i = 0; i < 50; i++)
+    {
+        cout << i << ": " << array[i] << endl;
+    }
+#endif
+#ifdef DEBUG_BUB
+    double arr1 [] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    
+    cout << "presort" << endl;
+    for(int i = 0; i < 10; i++)
+    {
+        cout << i << ": " << arr1[i] << endl;
+    }
+    BubbleSort(arr1, 10);
+    cout << "post sort" << endl;
+    for(int i = 0; i < 10; i++)
+    {
+        cout << i << ": " << arr1[i] << endl;
+    }
+#endif
+#ifdef DEBUG_QUICK
+    double arr[50];
+    srand(4331248);
+    double y;
+    for(int i = 0; i < 50; i++)
+    {
+        y = rand() % 99900 + 100;
+        arr[i] = y;
     }
     cout << "presort" << endl;
     for(int i = 0; i < 50; i++)
     {
         cout << i << ": " << arr[i] << endl;
     }
-    InsertionSort(arr, 50);
+    QuickSort(arr, 0, 49, MIDDLE);//, MEDIAN
     cout << "postsort" << endl;
     for(int i = 0; i < 50; i++)
     {
         cout << i << ": " << arr[i] << endl;
     }
 #endif
-
-    double arr [] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    
-    cout << "presort" << endl;
-    for(int i = 0; i < 10; i++)
-    {
-        cout << i << ": " << arr[i] << endl;
-    }
-    BubbleSort(arr, 10);
-    cout << "post sort" << endl;
-    for(int i = 0; i < 10; i++)
-    {
-        cout << i << ": " << arr[i] << endl;
-    }
 }
--- Assignments/Assignment3/sorts.h
+++ Assignments/Assignment3/sorts.h
@@ -4,46 +4,119 @@ using namespace std;
 
 //#define DEBUG
 
-void Swap(double arr[],int l, int r)
+static int quickComp, quickAssgn, insertComp, insertAssgn, bubSwaps;
+
+enum PivotType {
+    MIDDLE,
+    MEDIAN
+};
+
+bool QuickCompare(double x, double y)
+{
+    if(x > y)
+    {
+        quickComp++;
+        return true;
+    }
+    else
+    {
+        quickComp++;
+        return false;
+    }
+}
+
+bool InsertCompare(double x, double y)
+{
+    if(x > y)
+    {
+        insertComp++;
+        return true;
+    }
+    else
+    {
+        insertComp++;
+        return false;
+    }
+}
+
+void Swap(double* l, double* r)
 {
     double temp;
-    temp = arr[l];
-#ifdef DEBUG
-    cout << "temp: " << temp << endl;
-    cout << "arr[l]: " << arr[l] << endl;
-    cout << "arr[r]: " << arr[r] << endl;
-#endif
-    arr[l] = arr[r];
-#ifdef DEBUG
-    cout << "swapped arr[l]: " << arr[l] << endl;
-#endif
-    arr[r] = temp;
-#ifdef DEBUG
-    cout << "swapped arr[r]: " << arr[r] << endl;
-#endif
+    temp = *l;
+    *l = *r;
+    *r = temp;
 }
 
-void QuickSort(double arr[], int, int size)
+int Partition(double arr[], int low, int high, PivotType pivotType)
 {
-
+    int mid = (low + high) / 2;
+    int pivot = high; // initialize pivot variable
+    switch (pivotType)
+    {
+    case MIDDLE:
+        Swap(&arr[mid], &arr[high]);
+        quickAssgn++;
+        break;
+    case MEDIAN: // median algorithm adopted from mycplus.com/featured-articles/quicksort-in-c-programming-language/
+        // Chooses the median of the first, middle, and last elements as the pivot by comparing and ordering
+        if (QuickCompare(arr[low], arr[mid]))
+        {
+            Swap(&arr[low], &arr[mid]);
+            quickAssgn++;
+        }           
+        if (QuickCompare(arr[low], arr[high]))
+        {
+            Swap(&arr[low], &arr[high]);
+            quickAssgn++;
+        }
+        if (QuickCompare(arr[mid], arr[high]))
+        {
+            Swap(&arr[mid], &arr[high]);
+            quickAssgn++;
+        }
+        Swap(&arr[mid], &arr[high]);
+        quickAssgn++;
+        break;
+    }
+    int i = low - 1;
+    for (int j = low; j < high; j++)
+    {
+        quickComp++;
+        if (arr[j] <= arr[pivot])
+        {
+            i++;
+            Swap(&arr[i], &arr[j]);
+            quickAssgn++;
+        }
+    }
+    Swap(&arr[i+1], &arr[pivot]);
+    quickAssgn++;
+    return i+1;
 }
 
-int Partition(double arr[], int, int size)
+void QuickSort(double arr[], int low, int high, PivotType pivotType)
 {
 
+    if(low < high)
+    {
+        int pivot = Partition(arr, low, high, pivotType);//first partition
+        QuickSort(arr, low, pivot - 1, pivotType); // Sorts left of pivot
+        QuickSort(arr, pivot + 1, high, pivotType); // Sorts right of pivot
+    }
 }
 
 void BubbleSort(double arr[], int size)
 {
     for (int i = 0; i < size - 1; i++)
     {
-        bool noSwap = true;
+        bool noSwap = true;//ends sort early if array is sorted
         for (int j = 0; j < size - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 noSwap = false;
-                Swap(arr, j, j + 1);
+                Swap(&arr[j], &arr[j + 1]);
+                bubSwaps++;
             }
         }
         if(noSwap == true)
@@ -56,9 +129,10 @@ void InsertionSort(double arr[], int size)
     for(int i = 1; i < size; i++)
     {
         int j = i;
-        while(j > 0 && arr[j-1] > arr[j])
+        while(j > 0 && InsertCompare(arr[j-1], arr[j]))
         {
-            Swap(arr, j, j-1);
+            Swap(&arr[j], &arr[j-1]);
+            insertAssgn++;
             j = j - 1;
         }
     }
